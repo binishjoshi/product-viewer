6de32f60a6bd5d5806ab7da75cebaa4b9f84bce9
Neo <cyclonejet1@protonmail.com>
2022-11-20 06:58:39 +0545

    prettier

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,11 +5,14 @@ import { createUser } from '../service/user.service';
 import { Request, Response } from 'express';
 import { CreateUserInput } from '../schema/user.schema';
 
-export async function createUserHandler(req: Request<{}, {}, CreateUserInput["body"]>, res: Response) {
+export async function createUserHandler(
+  req: Request<{}, {}, CreateUserInput['body']>,
+  res: Response
+) {
   try {
     const user = await createUser(req.body);
-    return res.send(omit(user.toJSON(), "password"));
-  } catch(error: any) {
+    return res.send(omit(user.toJSON(), 'password'));
+  } catch (error: any) {
     logger.error(error);
     return res.status(409).send(error.message);
   }
--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 import config from 'config';
-import logger from './logger'
+import logger from './logger';
 
 async function connect() {
   const dbUri = config.get<string>('dbUri');
